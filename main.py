609f56424593e98cd717ff030def12cc6ed4be15
sravan A R <arsravan79@gmail.com>
2025-12-01 14:15:35 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+def main() ->list[int]:
+    """_summary_
+
+    :return: _description_
+    :rtype: list
+    """
+    _list = [random.randint(0,20) for i in range(50)]
+
+    return _list
+
+
